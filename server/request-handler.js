8b9841af91dd839766292c41a758d5135735b355
nicolasfujita <nickfujita@gmail.com>
2015-10-26 19:50:41 -0700

    Got the basic functionality working. PAssing all test cases. Now we are working on serving files.

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,48 +12,190 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+var fs = require('fs');
+var _ = require('underscore');
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log("Serving request type " + request.method + " for url " + request.url);
 
-  // The outgoing status.
-  var statusCode = 200;
+var _storage = [];
 
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end("Hello, World!");
+var insertNewMessage = function(message) {
+  message.createdAt = new Date();
+  message.objectId = JSON.stringify(message);
+  _storage.push(message);
 };
+
+var getMessages = function(room) {
+  if(!room){return _storage;}
+  var msgArr =  _.filter(_storage, function(value, key, list){
+    return value.roomname === room;
+  });
+  return JSON.stringify({results: msgArr});
+};
+
+// insertNewMessage({
+//       username: 'nick',
+//       text: 'hello everyone',
+//       roomname: 'homeroom'
+//     });
+
+console.log(_storage);
+
+var requestHandler = function(request, response) {
+  console.log('url: ' + request.url.split('?')[0] + '.');
+  var _response = {
+    status: 404,
+    headers: defaultCorsHeaders,
+    write: undefined,
+    end: undefined
+  };
+  // debugger; 
+  var parseUrl = function () {
+    var url = request.url;
+    url = url.split('?')[0];
+    if(url === '/'){return serveHtml('client/bbindex.html');}
+    urlSplit = url.split('/');
+    if(urlSplit[1] === 'classes') {
+      return serveAjax(urlSplit[2]);
+    }else{
+      return serveHtml('client' + url); 
+    }
+  };
+
+  var serveHtml = function (address) {
+    fs.readFile(address, function (err, data) {
+      if(!err){
+        
+      _response.status = 200;
+      _response.headers['Content-Type'] = "text/" + address.split('.')[1] || 'html';
+      response.writeHead(_response.status, _response.headers);
+      response.write(data);
+      response.end();
+      }
+    });
+  };
+
+var serveAjax = function (roomname) {
+  if(request.method === 'GET'){
+    _response.status = 200;
+    _response.headers['Content-Type'] = "application/json"; 
+    _response.end = getMessages(roomname);
+  }else if(request.method === 'POST'){
+    _response.status = 201;
+    _response.headers['Content-Type'] = "text/plain";
+    request.on('data', function (chunk) {
+      var msgObj = JSON.parse(chunk.toString());
+      if(!msgObj.roomname){ msgObj.roomname = "lobby";}
+      insertNewMessage(msgObj);
+    });
+
+  }
+};
+
+if(request.method === "OPTIONS"){
+  _response.status = 200;
+  _response.headers.Allow = 'GET,POST';
+}else{
+  parseUrl();
+}
+
+response.writeHead(_response.status, _response.headers);
+if(_response.write) response.write(_response.write);
+response.end? response.end(_response.end) : response.end();
+
+};
+
+//   console.log(request.method);
+
+//   var headers = defaultCorsHeaders;
+
+//   var url = request.url.split("/");
+
+
+//   var statusCode = 404;
+//   var outgoingBody = "404 - Not Found";
+//   console.log(request.url);
+//   switch(url[1]) {
+//     case '':
+//       console.log('serving status client files');
+//       console.log(request.url);
+
+//       fs.readFile('client/bbindex.html', function (err,data) {
+//         // if (err) {
+//         //   console.log(__dirname);
+//         //   console.log('error '+err);
+//         //   statusCode = 404;
+//         //   outgoingBody =JSON.stringify(err);
+//         //   return;
+//         // }
+//         // console.log(data.toString());
+//         statusCode = 200;
+//         headers['Content-Type'] = "text/html";
+//         response.writeHead(statusCode, headers);
+//         response.write(data);
+//         response.end();
+//         return;
+//         // outgoingBody = data;
+//       });
+
+//       break;
+//     case 'classes':
+//       if(url[2] === 'messages'){
+//         if(request.method === 'GET') {
+//           console.log('getting messages');
+//           statusCode = 200;
+//           headers['Content-Type'] = "application/json";
+//           outgoingBody = JSON.stringify({results:getMessages()});
+//         } else if(request.method === 'POST') {
+//           console.log('making new message');
+//           statusCode = 201;
+//           headers['Content-Type'] = "text/plain";
+//           request.on('data', function(chunk) {
+//             insertNewMessage(JSON.parse(chunk.toString()));
+//           });
+
+//           outgoingBody = 'inserted new message';
+//         } else if(request.method === 'OPTIONS') {
+//           statusCode = 200;
+//           outgoingBody = "LOOK AT THE DOCS YOU FOOL!";
+//           headers['Allow'] = 'GET,POST';
+//         }
+//         break;
+//       }else{
+//         if(request.method === 'GET') {
+//           statusCode = 200;
+//           headers['Content-Type'] = "application/json";
+//           outgoingBody = JSON.stringify({results:getMessages(url[2])});
+//         } else if(request.method === 'POST') {
+//           statusCode = 201;
+//           headers['Content-Type'] = "text/plain";
+//           request.on('data', function (chunk) {
+//             // console.log(chunk.toString);
+//             chunk = JSON.parse(chunk.toString());
+//             chunk.roomname = url[2];
+//             insertNewMessage(chunk);
+//           });
+//         }
+//       }
+//       break;
+//     case()
+//     default:
+//       fs.readFile('client/' + request.url, function (err, data) {
+//         response.writeHead(200, {'Content-Type': 'text/' + request.url.split('.').reverse()[0]});
+//         response.write(data);
+//         response.end();
+//         return; 
+//       })
+//       break;
+//   }
+
+
+
+
+//   // response.writeHead(statusCode, headers);
+//   // response.end(outgoingBody);
+
+// };
+
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
@@ -67,7 +209,46 @@ var requestHandler = function(request, response) {
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
+  "access-control-allow-headers": "content-type, accept, X-Parse-Application-Id, X-Parse-REST-API-Key",
   "access-control-max-age": 10 // Seconds.
 };
 
+
+
+exports.requestHandler = requestHandler;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // if(request.method === 'GET') {
+  //   if(request.url === '/classes/messages/'){
+  //     outgoingBody = JSON.stringify({results:getMessages()});
+  //   }
+  // } else if(request.method === 'POST') {
+  //   if(request.url === '/classes/messages/'){
+  //     request.on('data', function(chunk) {
+  //       console.log("Received body data:");
+  //       insertNewMessage(JSON.parse(chunk.toString()));
+  //     });
+  //     // insertNewMessage(request.data);
+  //     outgoingBody = 'inserted new message';
+  //   }
+  // }
