2ed1476f3f5e090195103b2eb605f9e63983e1a0
nicolasfujita <nickfujita@gmail.com>
2015-10-27 18:42:50 -0700

    logging for clusters

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ if (cluster.isMaster) {
     // Count the machine's CPUs
     var cpuCount = require('os').cpus().length;
 
+    console.log(require('os').cpus().length);
+
     // Create a worker for each CPU
     for (var i = 0; i < cpuCount; i += 1) {
         cluster.fork();
